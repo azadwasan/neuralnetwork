50c2a6e8301770fd181bd1ae5dbc1a53f6f3a25c
Azad <azad_wasan@hotmail.com>
2023-08-07 10:33:16 +0100

    Add legend to the plot

--- src/EasyNNPyScripts/PlotHypothesis.py
+++ src/EasyNNPyScripts/PlotHypothesis.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from matplotlib.patches import Patch
 
 def PlotHypothesis(ax, x1_coords, x2_coords, thetas, y_values, plotColor='blue', plotAlpha=0.5):
     x1_range = np.arange(min(x1_coords)-5, max(x1_coords)+5, 1)
@@ -16,9 +16,16 @@ def PlotHypothesis(ax, x1_coords, x2_coords, thetas, y_values, plotColor='blue',
     ax.set_ylabel('X2 - Feature 2')
     ax.set_zlabel('Y - Measurement')
 
-def CompareHypothesis(X, y, theta1, theta2):
+def CompareHypothesis(X, y, easyNNTheta, tensorFlowTheta):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    PlotHypothesis(ax, [row[0] for row in X], [row[1] for row in X], theta1, y)
-    PlotHypothesis(ax, [row[0] for row in X], [row[1] for row in X], theta2, y, plotColor='red')
+    PlotHypothesis(ax, [row[0] for row in X], [row[1] for row in X], easyNNTheta, y, plotColor='green')
+    PlotHypothesis(ax, [row[0] for row in X], [row[1] for row in X], tensorFlowTheta, y, plotColor='blue')
+    ## Create proxy artists for the legend
+    proxy1 = Patch(facecolor='green', edgecolor='green', alpha = 0.5, label='EasyNN gradient descent fit')
+    proxy2 = Patch(facecolor='blue', edgecolor='blue', alpha =0.5, label='Tensorflow gradient descent fit')
+
+    ## Add a legend to the plot using the proxy artists
+    ax.legend(handles=[proxy1, proxy2])
+
     plt.show()
