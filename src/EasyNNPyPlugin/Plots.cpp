f18829ea3c9415606288f3f70b7f61cf8d789bfb
Azad <azad_wasan@hotmail.com>
2023-08-16 21:00:36 +0100

    Allow optional extra parameters for comparison

--- src/EasyNNPyPlugin/Plots.cpp
+++ src/EasyNNPyPlugin/Plots.cpp
@@ -15,11 +15,11 @@ void Plots::CompareHypothesis(const std::vector<std::vector<double>>& X, const s
 	easyNN_unique_ptr pResult{ interpreter.executeMethod(scriptName, methodName, args.get()) };
 }
 
-void Plots::PlotClassificationData(const std::vector<std::vector<double>>& X, const std::vector<double>& y, const std::vector<double>& theta){
+void Plots::PlotClassificationData(const std::vector<std::vector<double>>& X, const std::vector<double>& y, const std::vector<double>& theta, const std::optional<std::vector<double>> theta2){
 	auto scriptName{ "PlotData" };
 	auto methodName{ "PlotClassificationData" };
 	auto& interpreter = PyInterpreter::getInstance();
-	easyNN_unique_ptr args{ interpreter.convertArgumentsToPyTuple(X, y, theta) };
+	easyNN_unique_ptr args{ interpreter.convertArgumentsToPyTuple(X, y, theta, theta2) };
 	easyNN_unique_ptr pResult{ interpreter.executeMethod(scriptName, methodName, args.get()) };
 	if (pResult == nullptr) {
 		PyErr_Print();
--- src/EasyNNPyPlugin/Plots.h
+++ src/EasyNNPyPlugin/Plots.h
@@ -1,13 +1,14 @@
 #ifndef PLOTS_H
 #define PLOTS_H
 #include <vector>
+#include <optional>
 
 namespace EasyNNPyPlugin {
 	class Plots
 	{
 	public:
 		static void CompareHypothesis(const std::vector<std::vector<double>>& X, const std::vector<double> y, const std::vector<double>& theta1, const std::vector<double>& theta2);
-		static void PlotClassificationData(const std::vector<std::vector<double>>& X, const std::vector<double>& y, const std::vector<double>& theta);
+		static void PlotClassificationData(const std::vector<std::vector<double>>& X, const std::vector<double>& y, const std::vector<double>& theta, const std::optional<std::vector<double>> theta2 = std::nullopt);
 	};
 }
 
